1a7aa9e152b487f644e7a900970fc1b05ff88902
innerren <innerren@gmail.com>
2019-08-01 10:09:13 +0300

    Updates

---
+++ precache-manifest.1c8ded659b36437b9e87b1562300124f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e76be05d038c6c04ac43a049a3e620",
+    "url": "/tabs-with-router/index.html"
+  },
+  {
+    "revision": "e2a1a79c0af50d22beba",
+    "url": "/tabs-with-router/static/css/main.fa39caac.chunk.css"
+  },
+  {
+    "revision": "9e5ec9885f218692c87d",
+    "url": "/tabs-with-router/static/js/2.41e6a074.chunk.js"
+  },
+  {
+    "revision": "e2a1a79c0af50d22beba",
+    "url": "/tabs-with-router/static/js/main.576fbc1a.chunk.js"
+  },
+  {
+    "revision": "3513456d47be6d2df32f",
+    "url": "/tabs-with-router/static/js/runtime~main.8e076b95.js"
+  }
+]);
